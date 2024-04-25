6849b063bd7ff4621572326cead3fda20026e6fd
Tushar Singh <104665356+Omega0411@users.noreply.github.com>
2024-04-26 01:11:24 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,452 @@
+import { css } from "@emotion/css";
+
+const Login = () => {
+  return (
+    <div
+      className={css`
+        width: 1366px;
+        border-radius: 24px;
+        background-color: var(--color-white);
+        max-width: 100%;
+        height: 768px;
+        overflow: hidden;
+        text-align: left;
+        font-size: var(--font-size-xs);
+        color: var(--color-gray-100);
+        font-family: var(--font-poppins);
+      `}
+    >
+      <div
+        className={css`
+          position: absolute;
+          top: 0px;
+          right: 0px;
+          background: linear-gradient(217.64deg);
+          width: 683px;
+          height: 768px;
+        `}
+      />
+      <img
+        className={css`
+          position: absolute;
+          top: 0px;
+          right: 0px;
+          width: 683px;
+          height: 768px;
+          object-fit: cover;
+        `}
+        alt=""
+        src="/rectangle-4@2x.png"
+      />
+      <div
+        className={css`
+          position: absolute;
+          top: calc(50% - 262px);
+          left: calc(50% + 136px);
+          backdrop-filter: blur(13.6px);
+          border-radius: 46px;
+          background-color: rgba(255, 255, 255, 0.21);
+          border: 1px solid rgba(255, 255, 255, 0.52);
+          box-sizing: border-box;
+          width: 412px;
+          height: 524px;
+        `}
+      />
+      <img
+        className={css`
+          position: absolute;
+          top: 122px;
+          right: 26px;
+          width: 785px;
+          height: 524px;
+          object-fit: cover;
+        `}
+        alt=""
+        src="/women-with-tab-1@2x.png"
+      />
+      <div
+        className={css`
+          position: absolute;
+          top: 149px;
+          left: 159px;
+          width: 364px;
+          height: 469px;
+        `}
+      >
+        <div
+          className={css`
+            position: absolute;
+            top: 301px;
+            left: 0px;
+            width: 364px;
+            height: 24px;
+            font-size: var(--font-size-base);
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              top: 0px;
+              left: 113px;
+            `}
+          >
+            <b className={css``}>Login</b>
+            <span
+              className={css`
+                color: var(--color-dimgray);
+              `}
+            >
+              {" "}
+              with Others
+            </span>
+          </div>
+          <img
+            className={css`
+              position: relative;
+              width: 364px;
+              height: 1px;
+            `}
+            alt=""
+            src="/subtract.svg"
+          />
+        </div>
+        <b
+          className={css`
+            position: absolute;
+            top: 0px;
+            left: 135px;
+            font-size: 30px;
+            text-transform: uppercase;
+            color: #000;
+          `}
+        >
+          Login
+        </b>
+        <div
+          className={css`
+            position: absolute;
+            top: 31px;
+            left: 14px;
+            font-size: var(--font-size-base);
+            color: var(--color-dimgray);
+          `}
+        >
+          How to i get started lorem ipsum dolor at?
+        </div>
+        <div
+          className={css`
+            position: absolute;
+            top: 149px;
+            left: 0px;
+            width: 364px;
+            height: 52px;
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              top: 0px;
+              left: 0px;
+              border-radius: var(--br-base);
+              background-color: var(--color-lavender-200);
+              width: 364px;
+              height: 52px;
+            `}
+          />
+          <div
+            className={css`
+              position: absolute;
+              top: 17px;
+              left: 48px;
+            `}
+          >
+            Password
+          </div>
+          <img
+            className={css`
+              position: absolute;
+              top: 14px;
+              left: 18px;
+              width: 24px;
+              height: 24px;
+              overflow: hidden;
+            `}
+            alt=""
+            src="/frame.svg"
+          />
+        </div>
+        <div
+          className={css`
+            position: absolute;
+            top: 79px;
+            left: 0px;
+            width: 364px;
+            height: 52px;
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              top: 0px;
+              left: 0px;
+              border-radius: var(--br-base);
+              background-color: var(--color-lavender-200);
+              width: 364px;
+              height: 52px;
+            `}
+          />
+          <div
+            className={css`
+              position: absolute;
+              top: 17px;
+              left: 48px;
+            `}
+          >
+            Username
+          </div>
+          <img
+            className={css`
+              position: absolute;
+              top: 14px;
+              left: 18px;
+              width: 24px;
+              height: 24px;
+              overflow: hidden;
+            `}
+            alt=""
+            src="/frame1.svg"
+          />
+        </div>
+        <div
+          className={css`
+            position: absolute;
+            top: 225px;
+            left: 120px;
+            width: 124px;
+            height: 52px;
+            color: var(--color-white);
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              top: 0px;
+              left: 0px;
+              box-shadow: 0px 8px 21px rgba(0, 0, 0, 0.16);
+              border-radius: var(--br-base);
+              background: linear-gradient(99.78deg);
+              width: 124px;
+              height: 52px;
+            `}
+          />
+          <b
+            className={css`
+              position: absolute;
+              top: 17px;
+              left: 30px;
+            `}
+          >
+            Login Now
+          </b>
+        </div>
+        <div
+          className={css`
+            position: absolute;
+            top: 349px;
+            left: 0px;
+            width: 364px;
+            height: 120px;
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              top: 0px;
+              left: 0px;
+              width: 364px;
+              height: 52px;
+            `}
+          >
+            <div
+              className={css`
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                border-radius: var(--br-base);
+                border: 1px solid var(--color-lavender-100);
+                box-sizing: border-box;
+                width: 364px;
+                height: 52px;
+              `}
+            />
+            <div
+              className={css`
+                position: absolute;
+                top: 11px;
+                left: 110px;
+                width: 144px;
+                height: 30px;
+              `}
+            >
+              <div
+                className={css`
+                  position: absolute;
+                  top: 6px;
+                  left: 38px;
+                `}
+              >
+                <span className={css``}>{`Login with `}</span>
+                <b className={css``}>google</b>
+              </div>
+              <img
+                className={css`
+                  position: absolute;
+                  top: 0px;
+                  left: 0px;
+                  width: 30px;
+                  height: 30px;
+                  object-fit: cover;
+                `}
+                alt=""
+                src="/google-1@2x.png"
+              />
+            </div>
+          </div>
+          <div
+            className={css`
+              position: absolute;
+              top: 68px;
+              left: 0px;
+              width: 364px;
+              height: 52px;
+            `}
+          >
+            <div
+              className={css`
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                border-radius: var(--br-base);
+                border: 1px solid var(--color-lavender-100);
+                box-sizing: border-box;
+                width: 364px;
+                height: 52px;
+              `}
+            />
+            <div
+              className={css`
+                position: absolute;
+                top: 11px;
+                left: 101px;
+                width: 162px;
+                height: 30px;
+              `}
+            >
+              <div
+                className={css`
+                  position: absolute;
+                  top: 6px;
+                  left: 38px;
+                `}
+              >
+                <span className={css``}>{`Login with `}</span>
+                <b className={css``}>Facebook</b>
+              </div>
+              <img
+                className={css`
+                  position: absolute;
+                  top: 0px;
+                  left: 0px;
+                  width: 30px;
+                  height: 30px;
+                  object-fit: cover;
+                `}
+                alt=""
+                src="/facebook-1@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={css`
+          position: absolute;
+          top: 469px;
+          left: 788px;
+          width: 79px;
+          height: 79px;
+        `}
+      >
+        <div
+          className={css`
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            border-radius: 50%;
+            background-color: var(--color-white);
+            width: 79px;
+            height: 79px;
+          `}
+        />
+        <img
+          className={css`
+            position: absolute;
+            top: 18px;
+            left: 19px;
+            width: 42px;
+            height: 42px;
+            object-fit: cover;
+          `}
+          alt=""
+          src="/thunderbolt-1@2x.png"
+        />
+      </div>
+      <b
+        className={css`
+          position: absolute;
+          top: 148px;
+          left: 850px;
+          font-size: 32px;
+          line-height: 46px;
+          color: var(--color-white);
+        `}
+      >
+        <p
+          className={css`
+            margin: 0;
+          `}
+        >
+          Very good
+        </p>
+        <p
+          className={css`
+            margin: 0;
+          `}
+        >
+          works are
+        </p>
+        <p
+          className={css`
+            margin: 0;
+          `}
+        >{`waiting for `}</p>
+        <p
+          className={css`
+            margin: 0;
+          `}
+        >
+          you Login
+        </p>
+        <p
+          className={css`
+            margin: 0;
+          `}
+        >
+          Now!!!
+        </p>
+      </b>
+    </div>
+  );
+};
+
+export default Login;
